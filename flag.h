8b18ce10862cdd155ff2c62ba7230a9352cf0c62
yaasin-raki2 <yassin23raki@gmail.com>
2021-09-30 11:55:07 +0100

    4th proto

--- flag.h
+++ flag.h
@@ -7,7 +7,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 #include <inttypes.h>
+#include <limits.h>
+#include <errno.h>
 
 typedef enum
 {
@@ -18,8 +21,8 @@ typedef enum
 
 bool *flag_bool(const char *name, bool def, const char *desc);
 uint64_t *flag_uint64(const char *name, uint64_t def, const char *desc);
-const char **flag_str(const char *name, const char *def, const char *desc);
-void flag_parse(int argc, char **argv);
+char **flag_str(const char *name, char *def, const char *desc);
+void flag_parse(int ac, char **av);
 void flag_print_help(FILE *stream);
 
 #endif // FLAG_H_
@@ -79,18 +82,90 @@ uint64_t *flag_uint64(const char *name, uint64_t def, const char *desc)
     return (uint64_t *)&flag->data;
 }
 
-const char **flag_str(const char *name, const char *def, const char *desc)
+char **flag_str(const char *name, char *def, const char *desc)
 {
     Flag *flag = flag_new(FLAG_STR, name, desc);
-    *((const char **)&flag->def) = def;
-    *((const char **)&flag->data) = def;
-    return (const char **)&flag->data;
+    *((char **)&flag->def) = def;
+    *((char **)&flag->data) = def;
+    return (char **)&flag->data;
 }
 
-void flag_parse(int argc, char **argv)
+static char *flag_shift_args(int *ac, char ***av)
 {
-    (void)argc;
-    (void)argv;
+    assert(*ac > 0);
+    char *result = **av;
+    *av += 1;
+    *ac -= 1;
+    return result;
+}
+
+void flag_parse(int ac, char **av)
+{
+    char *program = flag_shift_args(&ac, &av);
+    while (ac > 0)
+    {
+        char *flag = flag_shift_args(&ac, &av);
+        if (*flag != '-')
+        {
+            fprintf(stderr, "ERROR: %s: unkown flag\n", flag);
+            exit(1);
+        }
+        flag++;
+        size_t i = 0;
+        while (i < flags_count)
+        {
+            if (strcmp(flags[i].name, flag) == 0)
+            {
+                switch (flags[i].type)
+                {
+                case FLAG_BOOL:
+                {
+                    *(bool *)&flags[i].data = true;
+                }
+                break;
+                case FLAG_STR:
+                {
+                    if (ac == 0)
+                    {
+                        fprintf(stderr, "ERROR: %s: no argument provided\n", flag);
+                        exit(1);
+                    }
+                    char *arg = flag_shift_args(&ac, &av);
+                    *(char **)&flags[i].data = arg;
+                }
+                break;
+                case FLAG_UINT64:
+                {
+                    if (ac == 0)
+                    {
+                        fprintf(stderr, "ERROR: %s: no argument provided\n", flag);
+                        exit(1);
+                    }
+                    char *arg = flag_shift_args(&ac, &av);
+                    char *endptr;
+                    unsigned long long int result = strtoull(arg, &endptr, 10);
+                    if (arg == endptr || *endptr != '\0')
+                    {
+                        fprintf(stderr, "ERROR: %s: not a valid number\n", flag);
+                        exit(1);
+                    }
+                    if (result == ULLONG_MAX && errno == ERANGE)
+                    {
+                        fprintf(stderr, "ERROR: %s: 64 bit unsigned integer overflow\n", flag);
+                        exit(0);
+                    }
+                    *(uint64_t *)&flags[i].data = result;
+                }
+                break;
+                default:
+                    assert(0 && "unreachable");
+                    exit(69);
+                }
+            }
+            i++;
+        }
+    }
+    (void)program;
 }
 
 static char *flag_show_data(Flag_Type type, uintptr_t data)
@@ -103,18 +178,20 @@ static char *flag_show_data(Flag_Type type, uintptr_t data)
     {
         int n = snprintf(NULL, 0, "%" PRIu64, *(uint64_t *)&data);
         assert(n >= 0);
-        assert(flags_tmp_str_size + n <= FLAGS_TMP_STR_CAP);
+        assert(flags_tmp_str_size + n + 1 <= FLAGS_TMP_STR_CAP);
         int m = snprintf(flags_tmp_str + flags_tmp_str_size,
                          FLAGS_TMP_STR_CAP - flags_tmp_str_size,
                          "%" PRIu64,
                          *(uint64_t *)&data);
         assert(n == m);
-        flags_tmp_str_size += n;
+        flags_tmp_str_size += n + 1;
     }
     break;
     case FLAG_STR:
-        return *(const char **)&data;
+        return *(char **)&data;
     }
+    assert(0 && "unreachable");
+    exit(1);
 }
 
 void flag_print_help(FILE *stream)
@@ -122,10 +199,8 @@ void flag_print_help(FILE *stream)
     for (size_t i = 0; i < flags_count; i++)
     {
         fprintf(stream, "    -%s\n", flags[i].name);
-        fprintf(stream,
-                "    %s. (Default: %s)\n",
-                flags[i].desc,
-                flag_show_data(flags[i].type, flags[i].def));
+        fprintf(stream, "        %s.\n", flags[i].desc);
+        fprintf(stream, "        Default: %s\n", flag_show_data(flags[i].type, flags[i].def));
     }
 }
 
